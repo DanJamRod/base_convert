59298b5845127c03054eb83d817ca75e479aa8a6
Daniel James Rodgers <60154359+DanJamRod@users.noreply.github.com>
2020-06-07 17:10:47 +0100

    Update base_convert.py

--- base_convert.py
+++ base_convert.py
@@ -5,5 +5,15 @@ from base import dechex as dechex
 from base import hexdec as hexdec
 from base import hexbin as hexbin
 from base import binhex as binhex
+from base import addhex as addhex
+from base import multhex as multhex
+from base import divhex as divhex
+from base import subhex as subhex
+from base import exphex as exphex
+from base import addbin as addbin
+from base import multbin as multbin
+from base import divbin as divbin
+from base import subbin as subbin
+from base import expbin as expbin
 
 print()
