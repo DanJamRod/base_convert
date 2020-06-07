1e1ee0cf1cbe67255dfafef22bbc00034edece5a
Daniel James Rodgers <60154359+DanJamRod@users.noreply.github.com>
2020-06-07 16:42:36 +0100

    Create base_convert.py

---
+++ base_convert.py
@@ -0,0 +1,9 @@
+from base import base as base
+from base import decbin as decbin
+from base import bindec as bindec
+from base import dechex as dechex
+from base import hexdec as hexdec
+from base import hexbin as hexbin
+from base import binhex as binhex
+
+print()
