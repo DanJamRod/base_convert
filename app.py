0b11c3b56cce7ce5de9b8b80ea5a2c930008a896
Daniel James Rodgers <60154359+DanJamRod@users.noreply.github.com>
2020-07-14 12:47:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def index():
 
 @app.route('/convert',methods=['POST','GET'])
 def convert():
-    """ Convert between bases 
+    """ Convert between bases query
     """
     if request.method =='POST':
         session["convert_num"] = request.form["convert_num"]
@@ -36,18 +36,43 @@ def convert():
 def convert_result():
     """ Convert between bases result
     """
-    session["converted_num"] = base(int(session["convert_num"]), int(session["base_from"]), int(session["base_to"]))
+    session["converted_num"] = base(str(session["convert_num"]), int(session["base_from"]), int(session["base_to"]))
     try:
         return render_template("convert_result.html")
     except:
         return render_template('error.html')
 
-@app.route('/operation')
+@app.route('/operation',methods=['POST','GET'])
 def operation():
-    """ Perform an operation in any base
+    """ Operations on numbers with different bases query 
+    """
+    if request.method =='POST':
+        session["num_1"] = request.form["num_1"]
+        session["base_1"] = request.form["base_1"]
+        session["operation"] = request.form["operation"]
+        session["num_2"] = request.form["num_2"]
+        session["base_2"] = request.form["base_2"]
+        session["base_3"] = request.form["base_3"]
+        return redirect(url_for("operation_result"))
+    else:
+        return render_template("operation.html")
+
+
+@app.route('/operation_result')
+def operation_result():
+    """ Operations on numbers with different bases result
     """
     try:
-        text = "Placeholder"
-        return render_template("convert.html")
+        if session["operation"] == "^":
+            session["operation_result"] = base(int(base(str(session["num_1"]), int(session["base_1"]), 10)) ** int(base(str(session["num_2"]), int(session["base_2"]), 10)), 10, int(session["base_3"]))
+        if session["operation"] == "*":
+            session["operation_result"] = base(int(base(str(session["num_1"]), int(session["base_1"]), 10)) * int(base(str(session["num_2"]), int(session["base_2"]), 10)), 10, int(session["base_3"]))
+        if session["operation"] == "/":
+            session["operation_result"] = f'{base(int(base(str(session["num_1"]), int(session["base_1"]), 10)) // int(base(str(session["num_2"]), int(session["base_2"]), 10)), 10, int(session["base_3"]))}r{base(int(base(str(session["num_1"]), int(session["base_1"]), 10)) % int(base(str(session["num_2"]), int(session["base_2"]), 10)), 10, int(session["base_3"]))}'
+        if session["operation"] == "+":
+            session["operation_result"] = base(int(base(str(session["num_1"]), int(session["base_1"]), 10)) + int(base(str(session["num_2"]), int(session["base_2"]), 10)), 10, int(session["base_3"]))
+        if session["operation"] == "-":
+            session["operation_result"] = base(int(base(str(session["num_1"]), int(session["base_1"]), 10)) - int(base(str(session["num_2"]), int(session["base_2"]), 10)), 10, int(session["base_3"]))                                            
+        return render_template("operation_result.html")
     except:
         return render_template('error.html')
