62c41500dd17d58266c0627005b71600b1a65977
Daniel James Rodgers <60154359+DanJamRod@users.noreply.github.com>
2020-07-14 11:29:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,17 +19,30 @@ def index():
     else:
         return render_template("index.html")
 
-@app.route("/convert")
+@app.route('/convert',methods=['POST','GET'])
 def convert():
-    """ Convert between bases
+    """ Convert between bases 
     """
-    try:
-        text = "Placeholder"
+    if request.method =='POST':
+        session["convert_num"] = request.form["convert_num"]
+        session["base_from"] = request.form["base_from"]
+        session["base_to"] = request.form["base_to"]
+        return redirect(url_for("convert_result"))
+    else:
         return render_template("convert.html")
+
+
+@app.route('/convert_result')
+def convert_result():
+    """ Convert between bases result
+    """
+    session["converted_num"] = base(int(session["convert_num"]), int(session["base_from"]), int(session["base_to"]))
+    try:
+        return render_template("convert_result.html")
     except:
         return render_template('error.html')
 
-@app.route("/operation")
+@app.route('/operation')
 def operation():
     """ Perform an operation in any base
     """
