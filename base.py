e350763f5f58c85931afa02c1ca53ee82a0e86a3
Daniel James Rodgers <60154359+DanJamRod@users.noreply.github.com>
2020-07-14 12:47:29 +0100

    Update base.py

--- base.py
+++ base.py
@@ -19,35 +19,13 @@ def base(num, old_base, new_base):
         dec_num += dec_digit(digit) * old_base ** exp
         exp -= 1
     new_len = 1
-    while dec_num >= new_base**new_len:
-        new_len += 1
-    new_num = []
-    for i in range(new_len):
-        new_num.append(dec_num//new_base**(new_len - 1 - i))
-        dec_num -= new_num[i]*new_base**(new_len - 1 - i)
-    return "".join(digit_dec(digit) for digit in new_num)
-
-def addhex(num1, num2):
-    """ Adds two hex numbers
-    """
-    return dechex(int(hexdec(num1)) + int(hexdec(num2)))
-
-def multhex(num1, num2):
-    """ Multiplies two hex numbers
-    """
-    return dechex(int(hexdec(num1)) * int(hexdec(num2)))
-
-def subhex(num1, num2):
-    """ Subtracts two hex numbers
-    """
-    return dechex(int(hexdec(num1)) - int(hexdec(num2)))
-
-def divhex(num1, num2):
-    """ Divides two hex numbers
-    """
-    return f"{dechex(int(hexdec(num1)) // int(hexdec(num2)))}r{dechex(int(hexdec(num1)) % int(hexdec(num2)))}"
-
-def exphex(num1, num2):
-    """ Finds hex_num1^hex_num2
-    """
-    return dechex(int(hexdec(num1)) ** int(hexdec(num2)))
+    if new_base == 1:
+        return "1" * dec_num 
+    else:
+        while dec_num >= new_base**new_len:
+            new_len += 1
+        new_num = []
+        for i in range(new_len):
+            new_num.append(dec_num//new_base**(new_len - 1 - i))
+            dec_num -= new_num[i]*new_base**(new_len - 1 - i)
+        return "".join(digit_dec(digit) for digit in new_num)
